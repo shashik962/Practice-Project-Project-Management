be5254c7a3495c3130c4ac93081d15cd56c1f6ff
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-28 00:07:45 +0530

    Managing Tasks & Understanding Prop Drilling

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,27 @@ import SelectedProject from "./components/SelectedProject";
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
-    projects: []
+    projects: [],
+    tasks: []
   });
+
+  function handleAddTask(text) {
+    setProjectsState(prevState => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId,
+      }
+
+      return {
+        ...prevState,
+        tasks: [newTask, ...prevState.tasks]
+      }
+
+    });
+  }
+  function handleDeleteTask() {}
 
   function handleSelectProject(id) {
     setProjectsState(prevState => {
@@ -68,7 +87,15 @@ function App() {
 
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
 
-  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />;
+  let content = (
+    <SelectedProject 
+      project={selectedProject} 
+      onDelete={handleDeleteProject} 
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={projectsState.tasks}
+    />
+  );
 
   if(projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />;
--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,6 +1,6 @@
 import Tasks from "./Tasks";
 
-export default function SelectedProject({project, onDelete}) {
+export default function SelectedProject({ project, onDelete, onAddTask, onDeleteTask, tasks }) {
    
    const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
       year: 'numeric',
@@ -20,7 +20,7 @@ export default function SelectedProject({project, onDelete}) {
             <p className="mb-4 text-stone-400">{formattedDate}</p>
             <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
          </header>
-         <Tasks />
+         <Tasks onAdd={onAddTask}  onDelete={onDeleteTask} tasks={tasks}/>
       </div>
    );
 }
