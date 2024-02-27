452b09c9730b22cd01dfdc4113c6ec7e259a798c
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-28 00:22:50 +0530

    Clearing Tasks & Fixing Minor Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,14 @@ function App() {
 
     });
   }
-  function handleDeleteTask() {}
+  function handleDeleteTask(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id)
+      }
+    });
+  }
 
   function handleSelectProject(id) {
     setProjectsState(prevState => {
@@ -111,6 +118,7 @@ function App() {
         onStartAddProject={handleStartAddProject}  
         projects={projectsState.projects}
         onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId}
       />
       {content}
     </main>
