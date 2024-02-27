92607f1224908ef578164be271e5cdb071fd5526
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-27 21:24:37 +0530

    Styling the Modal via Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -35,11 +35,20 @@ function App() {
     });
   }
 
+  function handleCancelAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      }
+    });
+  }
+
   console.log(projectsState);
 
   let content;
   if(projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />;
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />;
   }else if(projectsState.selectedProjectId === undefined) {
     content =  <NoProjectSelected  onStartAddProject={handleStartAddProject} />;
   }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 import Input from "./Input.jsx";
 import Modal from './Modal.jsx';
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
    const modal = useRef();
 
    const title = useRef();
@@ -32,14 +32,14 @@ export default function NewProject({ onAdd }) {
    return (
       <>
          <Modal ref={modal} buttonCaption="close"> 
-            <h2>Invalid Input</h2>
-            <p>Oops... looks like you forget to enter a value.</p>
-            <p>Pleae make sure you provide a valid value for every input field.</p>
+            <h2 className='text-xl font-bold text-stone-700 my-4'>Invalid Input</h2>
+            <p className='text-stone-600 mb-4'>Oops... looks like you forget to enter a value.</p>
+            <p className='text-stone-600 mb-4'>Pleae make sure you provide a valid value for every input field.</p>
          </Modal>
          <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
                <li>
-                  <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+                  <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>Cancel</button>
                </li>
                <li>
                   <button 
