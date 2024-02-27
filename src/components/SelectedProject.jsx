37f58b058e5d74ad30aa9b000b9c7903c1cac668
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-27 23:21:45 +0530

    Adding "Project Tasks" & A Tasks Component

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,3 +1,5 @@
+import Tasks from "./Tasks";
+
 export default function SelectedProject({project, onDelete}) {
    
    const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
@@ -18,7 +20,7 @@ export default function SelectedProject({project, onDelete}) {
             <p className="mb-4 text-stone-400">{formattedDate}</p>
             <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
          </header>
-         
+         <Tasks />
       </div>
    );
 }
